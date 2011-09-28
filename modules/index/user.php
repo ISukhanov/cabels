3c4270818e815e9c211e32597b1f832c7c35565a
Igor A. Sukhanov <i.sukhanov@orsn-corp.ru>
2011-09-28 23:22:28 +0400

    Код для тестирования шаблонов

--- modules/index/user.php
+++ modules/index/user.php
@@ -4,6 +4,18 @@
 	class Module_Index extends Common_Index
 	{
 
+		public function defaultAction()
+		{
+			$out = new OutControl($this->modname, __FUNCTION__);
+
+			$out->addParam('test', array(
+				'type'  =>  OutControl::TAG_VALUE,
+				'value' =>  'test',
+			));
+
+		    return $out;
+		}
+
 	}
 
 
