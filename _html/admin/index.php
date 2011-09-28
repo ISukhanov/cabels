0b5db4e9f78b12581930020737256f0f76645c67
Igor A. Sukhanov <i.sukhanov@orsn-corp.ru>
2011-09-28 22:39:11 +0400

    Модуль index для админа

--- _html/admin/index.php
+++ _html/admin/index.php
@@ -1,6 +1,6 @@
 <?php
 
-	include dirname(__FILE__) .'/../kernel/kernel.php';
+	include dirname(__FILE__) .'/../../kernel/kernel.php';
 	$CMS = new Kernel();
 	if ($CMS->callModule('index', null, null, 'admin'))
 	{
---
+++ modules/index/admin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: ReBeL
+ * Date: 28.09.11
+ * Time: 22:36
+ */
+	require_once 'common.php';
+
+	class Module_Index extends Common_Index
+	{
+		
+	}
+
+?>
